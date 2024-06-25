46e55279a9e28cda40433067fa4fa1aacb9b1393
harrissondutra <harrissondutra@gmail.com>
2024-06-25 11:18:35 -0300

    Implementação da Toolbar

---
+++ app/src/main/java/com/example/appcinema/HomeScreen.kt
@@ -0,0 +1,7 @@
+package com.example.appcinema
+
+import androidx.compose.ui.Modifier
+
+fun HomeScreen(){
+
+}
--- app/src/main/java/com/example/appcinema/MainActivity.kt
+++ app/src/main/java/com/example/appcinema/MainActivity.kt
@@ -4,44 +4,191 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Movie
+import androidx.compose.material.icons.filled.MovieCreation
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.appcinema.ui.theme.APPCinemaTheme
+import com.example.appcinema.ui.theme.Black
+import com.example.appcinema.ui.theme.White
 
 class MainActivity : ComponentActivity() {
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             APPCinemaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                App()
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App() {
+    val scrollBehavior =
+        TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Black,
+                    actionIconContentColor = White,
+                    navigationIconContentColor = White,
+                ),
+                title = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(id = R.drawable.cinema),
+                            contentDescription = "Popcorn Icon",
+                            tint = Color.Unspecified
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text("Cine IREDE", color = White)
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    var expanded by remember { mutableStateOf(false) }
+                    IconButton(onClick = {expanded = true}, Modifier.padding(10.dp)) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                        DropdownMenuItem(
+                            text = {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Icon(imageVector = Icons.Filled.Home, contentDescription = null)
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text("Home")
+                                }
+                            },
+                            onClick = {
+                                // Handle click
+                            }
+                        )
+
+                        DropdownMenuItem(
+                            text = {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Icon(imageVector = Icons.Filled.Movie, contentDescription = null)
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text("Filmes")
+                                }
+                            },
+                            onClick = {
+                                // Handle click
+                            }
+                        )
+                        DropdownMenuItem(
+                            text = {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Icon(painterResource(id = R.drawable.popcorn), contentDescription = null)
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text("Séries")
+                                }
+                            },
+                            onClick = {
+                                // Handle click
+                            }
+                        )
+                        DropdownMenuItem(
+                            text = {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text("Ir para Favoritos")
+                                }
+                            },
+                            onClick = {
+                                // Handle click
+                            }
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+    ) { innerPadding ->
+        Box(Modifier.padding(paddingValues = innerPadding)) {
+            ScrollContent(innerPadding)
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+/*fun getMenu() {
+    val menu: List<String> = listOf("Menu", "Filmes", "Séries")
+     ScrollContent(menu)
+}*/
+
 @Composable
-fun GreetingPreview() {
-    APPCinemaTheme {
-        Greeting("Android")
+fun ScrollContent(innerPadding: PaddingValues) {
+    val listMenu: List<String> = listOf("Jiraya", "Jaspion", "He-Man")
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = innerPadding,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(listMenu.size) { index ->
+            Text(text = " ${listMenu[index]}")
+        }
     }
 }
